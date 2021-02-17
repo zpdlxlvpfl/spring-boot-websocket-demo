1232e75584886446e018e07eeb9f26939374c40e
st <zpdlxlvpfl@naver.com>
2021-02-17 11:21:44 +0900

    websokcet

---
+++ src/test/java/com/example/springbootwebsocketdemo/SpringBootWebsocketDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootwebsocketdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebsocketDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
